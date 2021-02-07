966d4313b23bbdd311c06087881ebfd3809208a9
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-07 22:04:13 +0100

    Update While_Loop.java
    Controlling the execution flow of a Java program done with a while loop.

--- While_Loop.java
+++ While_Loop.java
@@ -1,14 +1,14 @@
 public class While_Loop {
     static boolean condition() {
-       boolean result = Math.random() < 0.99;                // "Result" is a boolean so it can only
-       System.out.println(result + ", ");                    // return true or false. A random boolean is true 70% of the time
-       return result;                                        // and therefore false the remaining 30% of the time.
+       boolean result = Math.random() < 0.99;                // "Result" is a boolean so it can only return true or false. 
+       System.out.println(result + ", ");                    // A random boolean is true 70% of the time and therefore false the remaining 30% of the time.
+       return result;                                       
     }
     
     public static void main(String[] args) {
-        while(condition())                                   // This means: While condition() evaluates to "true" exectute the body of the loop
-            System.out.println("Inside the while loop.");    // Returns so long as condition() evaluates to true
+        while(condition())                                   // This means: While condition() evaluates to "true" exectute the body of the loop.
+            System.out.println("Inside the while loop.");    // Returns so long as condition() evaluates to true.
         
-        System.out.println("Outside of the while loop.");    // Returns when condition() evaluates to false
+        System.out.println("Outside of the while loop.");    // Returns when condition() evaluates to false.
     }
 }
