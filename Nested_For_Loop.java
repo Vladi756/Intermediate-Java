6cefb1e4ddc0b2a5d85c033acb53a61abb177631
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-08 13:02:11 +0100

    Nested_For_Loop
    A little bit more complex. Program uses nested for loops as well as an if statement to print prime numbers.

--- Nested_For_Loop.java
+++ Nested_For_Loop.java
@@ -1,11 +1,11 @@
 public class Nested_For_Loop {
     public static void main(String[] args) {
-	for(int i = 1; i < 1000; i++ ) {                    // Outter for loop determines the range of numbers the program outputs
-            int factors = 0;                                // The number of factors determines whether a number is prime or not, less than 2 = prime
-            for(int j = 1; j < (i + 2)/2; j++ ) {           // Inner for loop tests the number to find out how many factors it has
+	for(int i = 1; i < 1000; i++ ) {                    // Outter for loop determines the range of numbers the program outputs.
+            int factors = 0;                                // The number of factors determines whether a number is prime or not, less than 2 = prime.
+            for(int j = 1; j < (i + 2)/2; j++ ) {           // Inner for loop tests the number to find out how many factors it has.
 		if((i % j) == 0) factors++; 			
             }
-		if(factors < 2)                             // Less than 2 factors mean a number is prime (counting starts at 0)
+		if(factors < 2)                             // Less than 2 factors mean a number is prime (counting starts at 0).
                     System.out.println(i + " is prime");
 	}
     }
