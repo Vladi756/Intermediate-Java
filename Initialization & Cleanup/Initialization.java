2af1749356ad838383b9d6876cadf334971be661
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-09 21:50:16 +0100

    Initialization
    An introduction to the concept of initialization in Java.

--- Initialization & Cleanup/Initialization.java
+++ Initialization & Cleanup/Initialization.java
@@ -9,10 +9,10 @@ class Test {
 
 public class Initialization {
 	public static void main(String[] args) {
-		Test t = new Test();                    // Creates a new object with class "Test".
+		Test t = new Test();                    	// Creates a new object with class "Test".
 		System.out.println("t.s1: " + t.s1);    
 		System.out.println("t.s2: " + t.s2);
-		System.out.println("t.s3: " + t.s3);    // Outputting all 3 strings of 't'.
+		System.out.println("t.s3: " + t.s3);    	// Outputting all 3 strings of 't'.
 	}
 }
 
@@ -21,4 +21,8 @@ t.s1: null
 t.s2: hello
 t.s3: good-bye */ 
 
-/* No variable can ever be completely uninitialized in Java. Unlike C, when the progrmamer forgets to initialize a variable, Java simply initializes it for him by default, Java initialies the variable to "null", this is why, when outputted, t.s1 is "null". 's2' is quite straightforward - it is declared and initalized on line 3, all in one go. Finally, t.s3 uses a constructor, namely : Test(). A constuctor is a built in method in Java which is used to initialize any uninitialized variable. This is called a default constructor as it takes no arguments (constructors can also take arguments). Java does all this behind the scenes in order to guarantee initialization - however we as programmers can also manually call the constructor - as is the case with s3. */
+/* No variable can ever be completely uninitialized in Java. Unlike C, when the progrmamer forgets to initialize a variable, Java simply initializes it for him by default, 
+Java initialies the variable to "null", this is why, when outputted, t.s1 is "null". 's2' is quite straightforward - it is declared and initalized on line 3, all in one go. 
+Finally, t.s3 uses a constructor, namely : Test(). A constuctor is a built in method in Java which is used to initialize any uninitialized variable. 
+This is called a default constructor as it takes no arguments (constructors can also take arguments). The arguments would specify how an object is created. 
+Java does all this behind the scenes in order to guarantee initialization - however we as programmers can also manually call the constructor - as is the case with s3. */
