eab65b36e8c01b32b02c991b6ea278f30e4be04d
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-10 22:36:00 +0100

    Enums

---
+++ Initialization & Cleanup/Enums.java
@@ -0,0 +1,22 @@
+public class Enums {
+    public enum Currency {                      // Declaring and Initializing an enum data type
+        RSD, BGN, GBP, USD, EUR
+    }
+    
+    public static void main(String[] args) {
+        Currency money = Currency.USD;          
+        for(Currency m: Currency.values()) {        // Printing out every possible value of currency
+            System.out.println(m + " ordinal " + m.ordinal());  // Ordinal means position of value in the enum.
+        }
+    }
+}
+
+/* Output:
+
+RSD ordinal 0
+BGN ordinal 1
+GBP ordinal 2
+USD ordinal 3
+EUR ordinal 4
+
+*/
