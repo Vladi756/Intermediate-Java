91b3b18787d23f17cd514fe6d835251c152b7f0f
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-15 11:37:37 +0100

    Access_Specifiers
    An example of how access specifiers work in Java. A little reference in case you forget.

--- Access Control/Access_Specifiers.java
+++ Access Control/Access_Specifiers.java
@@ -1,8 +1,14 @@
 public class Access_Specifiers {
-	public static void main(String[] args) {
+	public static void main(String[] args) {			// ** THIS CODE DOES NOT WORK IT'S JUST AN EXAMPLE**
 		
-		public String s = "Hey";
-		private int i = 48;
-		protected boolean b = true; 
+		public class String {					// This class can be accessed by anyone
+			s = "Hey";
+		}							
+		private class int {					// This would act as a support class to public classes
+			i = 48;
+		}					
+		protected class boolean {				// This would inherit the classes from its parent classes while not changing them 
+			b = true;
+		} 
 	}
 }
