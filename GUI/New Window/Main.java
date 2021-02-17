ca07dc1722dce7d74cc372895a1fff3621e04b65
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-17 23:24:18 +0100

    Main
    This program simply creates an object of class LaunchPage.

--- GUI/New Window/Main.java
+++ GUI/New Window/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		LaunchPage launchPage = new LaunchPage();
+		LaunchPage launchPage = new LaunchPage();	// Creates an instance of LaunchPage().
 	}
 }
