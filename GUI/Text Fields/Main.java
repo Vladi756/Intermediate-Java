3aaadec83e9092717cdeab16af6ac05de63daf3e
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-18 18:39:03 +0100

    Main
    Program serves only to create an instance of the MyFrame class, in order to test to see whether it works.

---
+++ GUI/Text Fields/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// JTextField = A GUI textbok component that ca nbe used to add, set, or get text 
+		
+		MyFrame frame = new MyFrame();		// Creates an instance of MyFrame.
+	}
+}
