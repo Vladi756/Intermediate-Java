b3773490b078250402b7a34039585f8a7af94f90
Vladislav Kostic <vladislavkostic@yahoo.com>
2021-02-20 21:01:07 +0100

    ComboBox
    An example of JComboBox being used in Java.

--- GUI/ComboBox.java
+++ GUI/ComboBox.java
@@ -3,27 +3,27 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
-import javax.swing.JFrame;												// Necessary Imports
+import javax.swing.JFrame;								// Necessary Imports
 
 public class ComboBox extends JFrame implements ActionListener {			// Extends JFrame and Implement ActionListener
 	
-JComboBox comboBox;														// Declared in the global scope.
+JComboBox comboBox;									// Declared in the global scope.
 	
 
 	@SuppressWarnings("unchecked")
-	ComboBox() {															// Class constructor
+	ComboBox() {									// Class constructor
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
 		
-		String[] animals = {"cow", "sheep", "ox"};						// Has to be reference data type like strings. Primitives like int won't work
-		comboBox = new JComboBox(animals);								// Initialized in the constructor.
-		comboBox.addActionListener(this);								// Can pass "this" as the class impelements actionListener.
+		String[] animals = {"cow", "sheep", "ox"};				// Has to be reference data type like strings. Primitives like int won't work
+		comboBox = new JComboBox(animals);					// Initialized in the constructor.
+		comboBox.addActionListener(this);					// Can pass "this" as the class impelements actionListener.
 		
 		
 		this.add(comboBox);
 		this.pack();
 		this.setSize(500, 500);
-		this.setVisible(true);											// Setting up the JFrame.
+		this.setVisible(true);							// Setting up the JFrame.
 	}
 
 	@Override
